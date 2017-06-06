6a64a4064139672e62a72121815c987ef4aa5ceb
lbias <306508354@qq.com>
2017-06-06 09:36:08 +0800

    model event#to_param, generate_friendly_id

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,15 @@
 class Event < ApplicationRecord
   validates_presence_of :name
 
+  before_validation :generate_friendly_id, :on => :create
+
   def to_param
-    "#{self.id}-#{self.name}"
-  end 
+    self.friendly_id
+  end
+
+  protected
+
+  def generate_friendly_id
+    self.friendly_id ||= SecureRandom.uuid
+  end
 end
