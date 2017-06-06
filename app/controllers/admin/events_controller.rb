67a7b6b1dc68723bf5e9e92f36f85fc579a349fc
lbias <306508354@qq.com>
2017-06-06 15:37:21 +0800

    add category_id in controller admin::events#event_params

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -43,6 +43,6 @@ class Admin::EventsController < AdminController
   protected
 
   def event_params
-    params.require(:event).permit(:name, :description, :friendly_id, :status)
+    params.require(:event).permit(:name, :description, :friendly_id, :status, :category_id)
   end
 end
