81f06512487586c9e36dba4fba5a5a613ed071b1
lbias <306508354@qq.com>
2017-06-07 13:40:07 +0800

    relationship between model event and ticket

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,6 +8,7 @@ class Event < ApplicationRecord
   before_validation :generate_friendly_id, :on => :create
 
   belongs_to :category, :optional => true
+  has_many :tickets, :dependent => :destroy
 
   def to_param
     self.friendly_id
