a93c35e16550936a78bba6b9142ca9e466656cae
lbias <306508354@qq.com>
2017-06-08 11:00:54 +0800

    step2 controller registrations#step2, step2_update

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -16,6 +16,19 @@ class RegistrationsController < ApplicationController
     end
   end
 
+  def step2
+    @registration = @event.registrations.find_by_uuid(params[:id])
+  end
+
+  def step2_update
+    @registration = @event.registrations.find_by_uuid(params[:id])
+    if @registration.update(registration_params)
+      redirect_to step3_event_registration_path(@event, @registration)
+    else
+      render "step2"
+    end
+  end
+
   def show
     @registration = @event.registrations.find_by_uuid(params[:id])
   end
