cdc3c4369a663376ef7b115e73ff2223b9c7a90a
lbias <306508354@qq.com>
2017-06-08 11:27:02 +0800

    current_step in controller registrations#create, step1_update, step2_update, step3_update

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,6 +9,7 @@ class RegistrationsController < ApplicationController
     @registration.ticket = @event.tickets.find( params[:registration][:ticket_id] )
     @registration.status = "pending"
     @registration.user = current_user
+    @registration.current_step = 1
     if @registration.save
       redirect_to step2_event_registration_path(@event, @registration)
     else
@@ -22,6 +23,7 @@ class RegistrationsController < ApplicationController
 
   def step2_update
     @registration = @event.registrations.find_by_uuid(params[:id])
+    @registration.current_step = 2
     if @registration.update(registration_params)
       redirect_to step3_event_registration_path(@event, @registration)
     else
@@ -35,6 +37,7 @@ class RegistrationsController < ApplicationController
 
   def step3_update
     @registration = @event.registrations.find_by_uuid(params[:id])
+    @registration.current_step = 3
     @registration.status = "confirmed"
     if @registration.update(registration_params)
       flash[:notice] = "报名成功"
@@ -50,6 +53,7 @@ class RegistrationsController < ApplicationController
 
   def step1_update
     @registration = @event.registrations.find_by_uuid(params[:id])
+    @registration.current_step = 1
     if @registration.update(registration_params)
       redirect_to step2_event_registration_path(@event, @registration)
     else
