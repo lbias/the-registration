b9382866e1ce1a42f7a3b0e0da2034150ee3a99d
lbias <306508354@qq.com>
2017-06-10 10:01:09 +0800

    settings of uploaded files

--- app/uploaders/event_logo_uploader.rb
+++ app/uploaders/event_logo_uploader.rb
@@ -3,6 +3,7 @@ class EventLogoUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -33,7 +34,10 @@ class EventLogoUploader < CarrierWave::Uploader::Base
   # version :thumb do
   #   process resize_to_fit: [50, 50]
   # end
-
+  version :thumb do
+    process resize_to_fit: [50, 50]
+  end
+  
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_whitelist
