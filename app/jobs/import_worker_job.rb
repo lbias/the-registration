2183c4298a4b523353232575ba0a105a5d268ad5
lbias <306508354@qq.com>
2017-06-11 19:37:39 +0800

    debug

--- app/jobs/import_worker_job.rb
+++ app/jobs/import_worker_job.rb
@@ -2,7 +2,7 @@ class ImportWorkerJob < ApplicationJob
   queue_as :default
 
   def perform(import_id)
-    import = RegistrationImport.find(import_id
+    import = RegistrationImport.find(import_id)
     import.process!
   end
 end
