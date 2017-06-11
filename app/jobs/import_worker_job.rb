dfd14b6ff0287752191a7a9260a86e10ec67c289
lbias <306508354@qq.com>
2017-06-11 19:30:06 +0800

    import_worker_job#perform

--- app/jobs/import_worker_job.rb
+++ app/jobs/import_worker_job.rb
@@ -1,7 +1,8 @@
 class ImportWorkerJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform(import_id)
+    import = RegistrationImport.find(import_id
+    import.process!
   end
 end
