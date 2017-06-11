9ea58d19279d3dad4d9c927cf80cd5e93ed36bb1
lbias <306508354@qq.com>
2017-06-11 10:49:28 +0800

    ticket amountin controller admin::events#show

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -24,7 +24,18 @@ class Admin::EventsController < AdminController
           backgroundColor: colors,
           borderWidth: 1
         }]
-    }    
+    }
+
+    @data2 = {
+        labels: ticket_names,
+        datasets: [{
+            label: '# of Amount',
+            data:  @event.tickets.map{ |t| t.registrations.by_status("confirmed").count * t.price },
+            backgroundColor: colors,
+            borderWidth: 1
+        }]
+    }
+
   end
 
   def new
