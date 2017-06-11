49afd24edfc602e26acdeeeb2c9557bc208ece59
lbias <306508354@qq.com>
2017-06-11 16:47:58 +0800

    model user#is_admin?, is_editor?

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,12 @@ class User < ApplicationRecord
   def display_name
     self.email.split("@").first
   end
+
+  def is_admin?
+    self.role == "admin"
+  end
+
+  def is_editor?
+    ["admin", "editor"].include?(self.role)  # 如果是 admin 的话，当然也有 editor 的权限
+  end  
 end
